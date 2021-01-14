ff9c9150af51f91c6d422b16c7d8f93cac498d6b
Claudia Pfreundt <claudilein@gmail.com>
2021-01-14 22:45:10 +0100

    added watercooler. changed layout according to seating chart

---
+++ watercooler.tsx
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="watercooler" tilewidth="32" tileheight="32" tilecount="2" columns="1">
+ <image source="watercooler.png" width="33" height="66"/>
+ <tile id="0">
+  <properties>
+   <property name="collide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="collide" type="bool" value="true"/>
+  </properties>
+ </tile>
+</tileset>
