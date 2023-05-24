d85e6729d22fb1cf8e63f9c5616441d3dfe315f9
Claudia <claudilein@gmail.com>
2023-05-25 00:16:46 +0200

    Added dancefloor website back in. Removed Doko HowTo. Added some more writing for another room.

---
+++ Schrift_MakeItMeme_2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="Schrift_MakeItMeme_2" tilewidth="32" tileheight="32" tilecount="5" columns="5">
+ <image source="Schrift_MakeItMeme.png" trans="ffffff" width="185" height="32"/>
+</tileset>
