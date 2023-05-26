5eeb00258972d1973201a1330cfee42d44192a19
Claudia <claudilein@gmail.com>
2023-05-26 12:08:16 +0200

    re-added make it meme tileset

---
+++ Schrift_MakeItMeme_5.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="Schrift_MakeItMeme_5" tilewidth="32" tileheight="32" tilecount="11" columns="11">
+ <image source="Schrift_MakeItMeme.png" width="353" height="33"/>
+</tileset>
