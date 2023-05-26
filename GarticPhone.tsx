29e4b65fac32a337d723cd530a4532928a3a3c92
Claudia <claudilein@gmail.com>
2023-05-26 11:38:27 +0200

    added fonts for other game floors, removed covid impacts websites, added typeracer link, corrected typeracer scorebord link

---
+++ GarticPhone.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="GarticPhone" tilewidth="32" tileheight="32" tilecount="8" columns="8">
+ <image source="GarticPhone.png" width="277" height="35"/>
+</tileset>
---
+++ Schrift_Votesout.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="Schrift_Votesout" tilewidth="32" tileheight="32" tilecount="11" columns="11">
+ <image source="Votesout.png" width="353" height="35"/>
+</tileset>
